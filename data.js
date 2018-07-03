ac9c044fb099482ffba8dd514e7f3d6e8a8f7a47
Antoine Cusset <acusset@clever-age.com>
2018-07-03 12:52:41 +0200

    Add raw data and simple table

---
+++ data.js
@@ -0,0 +1,686 @@
+let data = [
+    {
+        "lap": 1,
+        "time": "16:11:26",
+        "duration": "11:18.31",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 2,
+        "time": "16:23:05",
+        "duration": "11:39.51",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 3,
+        "time": "16:34:16",
+        "duration": "11:10.95",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 4,
+        "time": "16:48:43",
+        "duration": "14:26.41",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 5,
+        "time": "16:59:37",
+        "duration": "10:54.11",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 6,
+        "time": "17:11:51",
+        "duration": "12:14.76",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 7,
+        "time": "17:24:13",
+        "duration": "12:21.26",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 8,
+        "time": "17:36:06",
+        "duration": "11:53.52",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 9,
+        "time": "17:49:46",
+        "duration": "13:39.47",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 10,
+        "time": "18:00:18",
+        "duration": "10:31.77",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 11,
+        "time": "18:11:03",
+        "duration": "10:45.60",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 12,
+        "time": "18:25:30",
+        "duration": "14:27.20",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 13,
+        "time": "18:35:05",
+        "duration": "09:34.97",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 14,
+        "time": "18:46:09",
+        "duration": "11:04.13",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 15,
+        "time": "18:58:19",
+        "duration": "12:09.84",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 16,
+        "time": "19:09:50",
+        "duration": "11:31.03",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 17,
+        "time": "19:23:16",
+        "duration": "13:26.17",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 18,
+        "time": "19:33:35",
+        "duration": "10:18.11",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 19,
+        "time": "19:43:57",
+        "duration": "10:22.87",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 20,
+        "time": "19:58:30",
+        "duration": "14:32.29",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 21,
+        "time": "20:08:00",
+        "duration": "09:30.77",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 22,
+        "time": "20:19:45",
+        "duration": "11:44.55",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 23,
+        "time": "20:31:32",
+        "duration": "11:46.57",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 24,
+        "time": "20:42:59",
+        "duration": "11:27.30",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 25,
+        "time": "20:56:27",
+        "duration": "13:28.14",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 26,
+        "time": "21:06:30",
+        "duration": "10:02.93",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 27,
+        "time": "21:17:00",
+        "duration": "10:30.28",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 28,
+        "time": "21:31:15",
+        "duration": "14:15.08",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 29,
+        "time": "21:40:37",
+        "duration": "09:21.80",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 30,
+        "time": "21:52:01",
+        "duration": "11:23.95",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 31,
+        "time": "22:03:54",
+        "duration": "11:53.20",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 32,
+        "time": "22:15:47",
+        "duration": "11:53.29",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 33,
+        "time": "22:29:59",
+        "duration": "14:11.81",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 34,
+        "time": "22:42:30",
+        "duration": "12:31.20",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 35,
+        "time": "22:58:03",
+        "duration": "15:32.69",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 36,
+        "time": "23:10:11",
+        "duration": "12:07.89",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 37,
+        "time": "23:25:35",
+        "duration": "15:24.15",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 38,
+        "time": "23:35:57",
+        "duration": "10:22.03",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 39,
+        "time": "23:46:41",
+        "duration": "10:44.04",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 40,
+        "time": "00:02:12",
+        "duration": "15:30.79",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 41,
+        "time": "00:11:57",
+        "duration": "09:45.42",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 42,
+        "time": "00:22:50",
+        "duration": "10:52.63",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 43,
+        "time": "00:33:44",
+        "duration": "10:53.80",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 44,
+        "time": "00:49:12",
+        "duration": "15:27.93",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 45,
+        "time": "00:59:14",
+        "duration": "10:02.66",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 46,
+        "time": "01:10:13",
+        "duration": "10:58.23",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 47,
+        "time": "01:21:07",
+        "duration": "10:54.35",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 48,
+        "time": "01:36:21",
+        "duration": "15:14.32",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 49,
+        "time": "01:47:46",
+        "duration": "11:24.18",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 50,
+        "time": "01:58:38",
+        "duration": "10:52.67",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 51,
+        "time": "02:09:46",
+        "duration": "11:08.05",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 52,
+        "time": "02:25:08",
+        "duration": "15:21.68",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 53,
+        "time": "02:36:25",
+        "duration": "11:16.69",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 54,
+        "time": "02:47:27",
+        "duration": "11:02.50",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 55,
+        "time": "02:58:38",
+        "duration": "11:10.99",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 56,
+        "time": "03:14:07",
+        "duration": "15:28.70",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 57,
+        "time": "03:25:55",
+        "duration": "11:47.86",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 58,
+        "time": "03:37:12",
+        "duration": "11:17.61",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 59,
+        "time": "03:48:30",
+        "duration": "11:17.55",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 60,
+        "time": "04:04:09",
+        "duration": "15:39.67",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 61,
+        "time": "04:16:24",
+        "duration": "12:14.87",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 62,
+        "time": "04:27:57",
+        "duration": "11:32.80",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 63,
+        "time": "04:39:35",
+        "duration": "11:38.10",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 64,
+        "time": "04:55:09",
+        "duration": "15:33.47",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 65,
+        "time": "05:07:46",
+        "duration": "12:36.87",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 66,
+        "time": "05:20:47",
+        "duration": "13:01.40",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 67,
+        "time": "05:33:14",
+        "duration": "12:26.70",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 68,
+        "time": "05:49:04",
+        "duration": "15:50.20",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 69,
+        "time": "06:01:49",
+        "duration": "12:44.74",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 70,
+        "time": "06:14:21",
+        "duration": "12:32.52",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 71,
+        "time": "06:26:54",
+        "duration": "12:32.74",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 72,
+        "time": "06:43:26",
+        "duration": "16:32.10",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 73,
+        "time": "06:56:59",
+        "duration": "13:32.64",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 74,
+        "time": "07:11:46",
+        "duration": "14:47.65",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 75,
+        "time": "07:24:38",
+        "duration": "12:51.76",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 76,
+        "time": "07:41:08",
+        "duration": "16:29.79",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 77,
+        "time": "07:55:09",
+        "duration": "14:01.34",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 78,
+        "time": "08:08:13",
+        "duration": "13:04.21",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 79,
+        "time": "08:20:58",
+        "duration": "12:44.98",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 80,
+        "time": "08:38:22",
+        "duration": "17:23.56",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 81,
+        "time": "08:51:24",
+        "duration": "13:02.49",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 82,
+        "time": "09:03:43",
+        "duration": "12:18.56",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 83,
+        "time": "09:16:23",
+        "duration": "12:40.40",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 84,
+        "time": "09:34:02",
+        "duration": "17:39.08",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 85,
+        "time": "09:47:21",
+        "duration": "13:18.36",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 86,
+        "time": "10:01:12",
+        "duration": "13:51.64",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 87,
+        "time": "10:13:32",
+        "duration": "12:19.93",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 88,
+        "time": "10:24:08",
+        "duration": "10:35.59",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 89,
+        "time": "10:34:46",
+        "duration": "10:37.85",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 90,
+        "time": "10:49:31",
+        "duration": "14:45.77",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 91,
+        "time": "10:59:53",
+        "duration": "10:21.77",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 92,
+        "time": "11:10:52",
+        "duration": "10:58.98",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 93,
+        "time": "11:22:12",
+        "duration": "11:20.17",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 94,
+        "time": "11:37:20",
+        "duration": "15:07.20",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 95,
+        "time": "11:48:43",
+        "duration": "11:23.74",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 96,
+        "time": "12:02:09",
+        "duration": "13:25.92",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 97,
+        "time": "12:16:23",
+        "duration": "14:13.99",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 98,
+        "time": "12:29:00",
+        "duration": "12:36.80",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 99,
+        "time": "12:45:36",
+        "duration": "16:35.59",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 100,
+        "time": "12:57:23",
+        "duration": "11:47.36",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 101,
+        "time": "13:08:37",
+        "duration": "11:13.88",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 102,
+        "time": "13:23:54",
+        "duration": "15:16.89",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 103,
+        "time": "13:36:05",
+        "duration": "12:11.31",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 104,
+        "time": "13:51:28",
+        "duration": "15:23.46",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 105,
+        "time": "14:04:19",
+        "duration": "12:50.81",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 106,
+        "time": "14:22:39",
+        "duration": "18:19.99",
+        "skatter": "Moufida"
+    },
+    {
+        "lap": 107,
+        "time": "14:33:54",
+        "duration": "11:14.36",
+        "skatter": "Guillaume"
+    },
+    {
+        "lap": 108,
+        "time": "14:45:23",
+        "duration": "11:29.08",
+        "skatter": "Antoine"
+    },
+    {
+        "lap": 109,
+        "time": "15:00:47",
+        "duration": "15:24.83",
+        "skatter": "Marie"
+    },
+    {
+        "lap": 110,
+        "time": "15:14:14",
+        "duration": "13:26.17",
+        "skatter": "Stéphane"
+    },
+    {
+        "lap": 111,
+        "time": "15:27:31",
+        "duration": "13:16.97",
+        "skatter": "Sahbi"
+    },
+    {
+        "lap": 112,
+        "time": "15:40:11",
+        "duration": "12:40.42",
+        "skatter": "Séverine"
+    },
+    {
+        "lap": 113,
+        "time": "15:52:00",
+        "duration": "11:49.32",
+        "skatter": "Laurent"
+    },
+    {
+        "lap": 114,
+        "time": "16:05:20",
+        "duration": "13:20.05",
+        "skatter": "Laurent"
+    }
+]
